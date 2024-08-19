f28e3cc0d25bcb7b0366088c892c543941e66e11
Ghaith Nahdi <121437369+TUNISIA-user@users.noreply.github.com>
2024-08-19 11:20:28 +0100

    Create loop.py
    loop__pyhton

---
+++ loop.py
@@ -0,0 +1,11 @@
+Loop in pyhton
+
+for Index in range(initialeValue,10,1) : 
+    print(Index) ;  
+
+
+i = 0 
+While (i< 10) : 
+     print("hello    world") 
+     i+=1 or i=i+1 its the   same  thing 
+
